0dee577eb98ed0795e59d07fe9e5f4f9c29ab1c3
AllanKariuki <a.kariuki@keshiholdings.com>
2025-02-02 20:20:46 +0300

    product categories

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    units_in_stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Product, ProductCategory
+from .serializers import ProductSerializer, ProductCategorySerializer
 
-# Create your views here.
+
+class ProductCategoryViewSet(viewsets.ModelViewSet):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+    def create(self, request):
+        try :
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({
+                'detail': 'Product Category created successfully',
+                'data': serializer.data,
+            })
+        except ProductCategory.AlreadyExists:
+            return Response({
+                'detail': 'Product Category already exists',
+            }, status=status.HTTP_400_BAD_REQUEST)
+    
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'detail': serializer.data}, status = status.HTTP_200_OK) 
+    
+    def retrieve(self, request, pk=None):
+        try:
+            queryset = self.get_queryset()
+            product_category = queryset.get(pk=pk)
+            serializer = self.get_serializer(product_category)
+            return Response({'detail': serializer.data}, status = status.HTTP_200_OK)
+        except ProductCategory.DoesNotExist:
+            return Response({'detail': 'Product Category not found'}, status=status.HTTP_404_NOT_FOUND)
