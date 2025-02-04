98daed54a330affa9f73dfbc38992abb495be536
AllanKariuki <a.kariuki@keshiholdings.com>
2025-02-04 20:09:41 +0300

    products routes

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductCategoryViewSet
+from .views import ProductCategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r'categories', ProductCategoryViewSet)
+router.register(r'', ProductViewSet)
 
 urlpatterns = [
     path('products/', include(router.urls)),
