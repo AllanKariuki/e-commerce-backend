63bd47dbe51f1fb8ce86b6967825edea1ab44c1c
AllanKariuki <a.kariuki@keshiholdings.com>
2025-02-13 08:26:56 +0300

    feat: order and order item creation and update as well as refinement of the order model

--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,6 @@ from products.models import Product
 # Create your models here.
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    products = models.ManyToManyField(Product, through='OrderItem')
     status=models.CharField(max_length=50, default='pending')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
