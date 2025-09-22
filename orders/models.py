a889eb0fc3e8199a5938de01707d2c995a681be3
AllanKariuki <a.kariuki@keshiholdings.com>
2025-09-22 15:32:57 +0300

    feat: add descriptions field that will have size and other information received

--- orders/models.py
+++ orders/models.py
@@ -18,6 +18,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
