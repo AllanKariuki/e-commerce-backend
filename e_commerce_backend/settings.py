a00e218a702a58a0a80d2bd02c6fd414e21012a6
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-02 11:23:07 +0300

    commented out pagination settings

--- e_commerce_backend/settings.py
+++ e_commerce_backend/settings.py
@@ -79,8 +79,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20  # Number of items per page
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 20  # Number of items per page
 }
 
 #Setting up keycloak to be the authentication backend
