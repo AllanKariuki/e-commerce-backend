3f65a403bb3481dde6bb5b020dcd986e2062367d
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-20 13:30:59 +0300

    feat: code clean up

--- products/views.py
+++ products/views.py
@@ -1,18 +1,3 @@
-# from django.shortcuts import render
-# from rest_framework import viewsets, status
-# from rest_framework.response import Response
-# from .models import Product, ProductCategory
-# from .serializers import ProductSerializer, ProductCategorySerializer
-
-
-# class ProductCategoryViewSet(viewsets.ModelViewSet):
-#     queryset = ProductCategory.objects.all()
-#     serializer_class = ProductCategorySerializer
-
-# class ProductViewSet(viewsets.ModelViewSet):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-
 from rest_framework import viewsets
 from django.db.models import Q
 from .models import Product, ProductCategory, ProductImage
