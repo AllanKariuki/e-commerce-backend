ecca56ff5ef225d46e4f8d797eb465206f71641f
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-20 16:16:34 +0300

    feat: global setting to decode redis results to string-failed

--- e_commerce_backend/settings.py
+++ e_commerce_backend/settings.py
@@ -269,6 +269,7 @@ CACHES = {
         "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "REDIS_CLIENT_KWARGS": {"decode_responses": True}
         }
     }
 }
